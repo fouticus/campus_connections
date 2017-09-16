bb301f0db9b160459d95cc085c21efb17517cb5d
Alex Fout <fout.alex@gmail.com>
2017-09-16 00:31:58 -0600

    added function to view individual relationships through time.

--- config.R
+++ config.R
@@ -23,9 +23,20 @@ role_colors = c("red", "green", "purple", "yellow", "blue")
 #roles = c("mentor", "mentee", "instr", "lead", "coach")
 gender2_colors = c("grey", "pink", "cyan")
 gender3_colors = c("pink", "cyan")
+dyad_colors = c("black", "red")
 
 
 #role_pallete <- brewer.pal(5,"Set1")
 role_pallete <- role_colors
 names(role_pallete) <- levels(participants$role)
 role_fill <- scale_fill_manual(name = "role", values = role_pallete)
+role_color <- scale_color_manual(name = "role", values = role_pallete)
+sender_role_fill <- scale_fill_manual(name = "sender_role", values = role_pallete)
+sender_role_color <- scale_color_manual(name = "sender_role", values = role_pallete)
+receiver_role_fill <- scale_fill_manual(name = "receiver_role", values = role_pallete)
+receiver_role_color <- scale_color_manual(name = "receiver_role", values = role_pallete)
+
+dyad_pallete <- dyad_colors
+names(dyad_pallete) <- c(FALSE, TRUE)
+dyad_fill <- scale_fill_manual(name = "dyad", values = dyad_pallete)
+dyad_color <- scale_color_manual(name = "dyad", values = dyad_pallete)
---
+++ edge_trajectories.R
@@ -0,0 +1,38 @@
+source("config.R")
+source("functions.R")
+
+# clean up edge lists
+valid_edges <- edges[with(edges, sender_missing==0 & receiver_missing==0 & sn1==1 & !is.na(sn2) & sender_final_id!=receiver_final_id), ]
+roles <- participants[,c("final_id", "role")]
+valid_edges <- merge(valid_edges, roles, by.x="sender_final_id", by.y="final_id")
+colnames(valid_edges)[length(colnames(valid_edges))] <- "sender_role"
+valid_edges <- merge(valid_edges, roles, by.x="receiver_final_id", by.y="final_id")
+colnames(valid_edges)[length(colnames(valid_edges))] <- "receiver_role"
+valid_edges$pair_id <- paste(valid_edges$sender_final_id, valid_edges$receiver_final_id, sep="->")
+valid_edges$dyad <- substr(valid_edges$sender_final_id, 2, 999) == substr(valid_edges$receiver_final_id, 2, 999)
+
+# ordering happens in the plot functions now
+#valid_edges <- valid_edges[with(valid_edges, order(semester, night, sender_final_id, receiver_final_id, survnum)),]
+
+
+semesters <- c("Fa15")
+nights <- c("Mon", "Tue", "Wed", "Thu")
+roles <- c("mentor", "mentee")
+
+point_size <- 1
+scale_factor <- 3
+
+for(semester_a in semesters){
+  for(night_a in nights){
+    for(role_a in roles){
+      #plot_outedges_byperson(valid_edges, semester_a, night_a, role_a, scale_factor, point_size)
+    }
+  }
+}
+for(semester_a in semesters){
+  for(night_a in nights){
+    for(role_a in roles){
+      plot_outedges_dyadonly(valid_edges, semester_a, night_a, role_a, scale_factor, point_size)
+    }
+  }
+}
