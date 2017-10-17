cbb9d32bb894fb81d1a320bca3b48057d575f78f
Alex Fout <fout.alex@gmail.com>
2017-10-16 20:26:02 -0600

    model as of convo with bailey

--- modeling.R
+++ modeling.R
@@ -29,7 +29,7 @@ create_indicators <- function(X){
   return(X)
 }
 
-X1 <- create_indicators(X)
+X1 <- create_indicators(X1)
 X0 <- create_indicators(X0)
 
 X1[,"edge"] <- 1
@@ -38,26 +38,62 @@ X0[,"edge"] <- 0
 X <- rbind(X0, X1)
 X <- X[complete.cases(X),]
 
+# generate weights
+n_edges <- sum(X[,"edge"] == 1)
+n_non_edges <- sum(X[,"edge"] == 0)
+n_examples <- n_edges + n_non_edges
+wt_edges <- round(n_non_edges/n_edges)
+X[,"weight"] <- X[,"edge"]*wt_edges + (1-X[,"edge"])
+
 set_types <- function(df){
+  df$sender_gender_X <- factor(df$sender_gender_X, levels=c(0, 1), labels=c("Male", "Female"))
+  df$receiver_gender_X <- factor(df$receiver_gender_X, levels=c(0, 1), labels=c("Male", "Female"))
   df$sender_role_X <- factor(df$sender_role_X, levels=c(0, 1), labels=c("mentee", "mentor"))
   df$receiver_role_X <- factor(df$receiver_role_X, levels=c(0, 1), labels=c("mentee", "mentor"))
   df$dyad_X <- factor(df$dyad_X, levels=c(0, 1), labels=c("non-dyad", "dyad"))
-  df$m_fam <- factor(df$m_fam, levels=c(0, 1), labels=c("mfam", "non-mfam"))
+  df$m_fam <- factor(df$m_fam, levels=c(0, 1), labels=c("non-mfam", "mfam"))
   return(df)
 }
 X <- set_types(X)
 
-m <- glm(edge~sender_gender_X+receiver_gender_X+sender_role_X*receiver_role_X+dyad_X*m_fam, family=binomial(link='logit'),data=X)
-m <- glm(edge~sender_role_X*receiver_role_X + dyad_X*m_fam + sender_role_X*dyad_X + receiver_role_X*dyad_X, family=binomial(link='logit'),data=X)
-m <- glm(edge~sender_role_X*receiver_role_X*dyad_X + dyad_X*m_fam, family=binomial(link='logit'),data=X)
+#m <- glm(edge~sender_role_X*receiver_role_X*dyad+dyad_X*m_fam, weights=weight, family=binomial(link='logit'),data=X)
+#m <- glm(edge~sender_gender_X*receiver_gender_X*sender_role_X*receiver_role_X*dyad_X*m_fam, weights=weight, family=binomial(link='logit'),data=X)
+m_null <- glm(edge~1, weights=weight, family=binomial(link='logit'),data=X)
+#m <- glm(edge~sender_gender_X*sender_role_X+receiver_gender_X*receiver_role_X+sender_role_X*receiver_role_X+dyad_X*m_fam, weights=weight, family=binomial(link='logit'),data=X)
+m <- glm(edge~sender_gender_X*sender_role_X+receiver_gender_X*receiver_role_X+sender_role_X*receiver_role_X+dyad_X*m_fam, family=binomial(link='logit'),data=X)
+#m <- glm(edge~dyad_X, weights=weight, family=binomial(link='logit'),data=X)
+#m <- glm(edge~sender_role_X*receiver_role_X+dyad_X*m_fam, family=binomial(link='logit'),data=X)
+#m <- glm(edge~sender_role_X*receiver_role_X + dyad_X*m_fam + sender_role_X*dyad_X + receiver_role_X*dyad_X, family=binomial(link='logit'),data=X)
+#m <- glm(edge~sender_role_X*receiver_role_X*dyad_X + dyad_X*m_fam, family=binomial(link='logit'),data=X)
+summary(m_null)
 summary(m)
 anova(m) 
+#exp(confint(m))
+mcfadden_r2 <- 1-logLik(m)/logLik(m_null)
+mcfadden_r2
 
-X_pred <- expand.grid(c(0,1), c(0,1), c(0,1), c(0,1))
-#X_pred <- as.data.frame(matrix(c(0, 1, 0, 0,   0, 1, 1, 0,   0, 1, 0, 1,   0, 1, 1, 1), 4, byrow=TRUE))
-#X_pred <- rbind(X_pred, matrix(c(1, 0, 0, 0,   1, 0, 1, 0,   1, 0, 0, 1,   1, 0, 1, 1), 4, byrow=TRUE))
-colnames(X_pred) <- c("sender_role_X", "receiver_role_X", "dyad_X", "m_fam")
+#scatter.smooth(jitter(predict(m),amount=0), jitter(residuals(m),amount=0), cex=0.1)
+#scatter.smooth(predict(m, type="response"), residuals(m, type="deviance"), span=0.1, cex=0.1, main="Estimated Probability vs Deviance")
+#plot(predict(m, type="response"), residuals(m, type="deviance"), cex=0.1, main="Estimated Probability vs Deviance")
+#loess(predict(m, type="response") ~ residuals(m))
+#scatter.smooth(predict(m), weighted.residuals(m), span=0.1, cex=0.1, main="Linear Predictor vs Deviance")
+simple_roc <- function(labels, scores){
+  labels <- labels[order(scores, decreasing=TRUE)]
+  data.frame(TPR=cumsum(labels)/sum(labels), FPR=cumsum(!labels)/sum(!labels), labels)
+}
+roc_df <- simple_roc(X$edge, fitted.values(m))
+ggplot(roc_df, aes(x=FPR, y=TPR)) + geom_point() + geom_abline(slope=1) + ggtitle("ROC Curve")
+#plot(m)
+
+X_obs <- aggregate(edge ~ sender_role_X + receiver_role_X + dyad_X + m_fam, data=X, FUN=mean)
+X_obs2 <- aggregate(edge ~ sender_role_X + receiver_role_X + dyad_X + m_fam, data=X, FUN=length)
+X_obs <- merge(X_obs, X_obs2, by=c("sender_role_X", "receiver_role_X", "dyad_X", "m_fam"))
+rm(X_obs2)
+X_obs
+
+X_pred <- expand.grid(c(0,1), c(0,1), c(0,1), c(0,1), c(0,1), c(0,1))
+colnames(X_pred) <- c("sender_role_X", "receiver_role_X", "dyad_X", "m_fam", "sender_gender_X", "receiver_gender_X")
 X_pred <- set_types(X_pred)
 
-X_pred[, "prob"] <- predict(m, X_pred[1:4])
+X_pred[, "prob"] <- predict(m, X_pred, type="response")
 X_pred
