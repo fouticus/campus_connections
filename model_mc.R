b20f2af04109ee48b7af480e609d1adfc77706f7
Alex Fout <fout.alex@gmail.com>
2017-11-15 00:42:46 -0700

    added class for generalized dyadic model and created function for sim. also added in computation of betweenness centrality in the summary stats function

--- model_mc.R
+++ model_mc.R
@@ -1,13 +1,15 @@
 set.seed(983182)
 
 
+N = 1000
+
 setRefClass(
   "GraphModel"
 )
 SBM <- R6Class(
   "StochBlockModel",
   public = list(
-    groups=2,
+    groups = c(5, 5),
     probs=matrix(c(1.0, 0.5, 0.5, 1.0), ncol=2),
     initialize = function(groups, probs){
       self$groups <- groups
@@ -36,48 +38,87 @@ SBM <- R6Class(
   )
 )
 
+GDI <- R6Class(
+  "GeneralizedDyadIndependent",
+  public = list(
+    groups = c(1, 1),
+    probs=matrix(c(1.0, 0.5, 0.5, 1.0), ncol=2),
+    initialize = function(groups, probs){
+      self$groups <- groups
+      self$probs <- probs
+    },
+    realize = function(){
+      n <- dim(self$probs)[1]
+      adj <- matrix(nrow=n, ncol=n)
+      # simulate all edges between types
+      adj <- matrix(runif(n*n), nrow=n, ncol=n) < self$probs
+      return(adj)
+    }
+  )
+)
 
-
-network_stats <- function(adj, groups=NA){
+network_stats <- function(adj, groups=NULL){
   # compute different statistics about a model
   n_stats <- new.env()
   n_stats$out_degrees <- rowSums(adj)
   n_stats$in_degrees <- colSums(adj)
   n_stats$ave_out_degree <- mean(n_stats$out_degrees)
   n_stats$ave_in_degree <- mean(n_stats$in_degrees)
-  if(!is.na(groups)){
+  if(!is.null(groups)){
     group_ave_out_degree <- vector(length=length(groups))
     group_ave_in_degree <- vector(length=length(groups))
     idx <- 1
     for(i in 1:length(groups)){
       n_g <- groups[i]
-      group_ave_out_degree[i] <- drop(rowSums(adj[idx:(idx+n_g-1),]))
-      group_ave_in_degree[i] <- drop(colSums(adj[,idx:(idx+n_g-1)]))
+      group_ave_out_degree[i] <- mean(drop(rowSums(adj[idx:(idx+n_g-1),])))
+      group_ave_in_degree[i] <- mean(drop(colSums(adj[,idx:(idx+n_g-1)])))
       idx <- idx + n_g
     }
     n_stats$group_ave_out_degree <- group_ave_out_degree
     n_stats$group_ave_in_degree <- group_ave_in_degree
   }
+  n_stats$CB <- betweenness_centrality(adj)
   return(n_stats)
 }
 
-#specify a graph model
-groups <- c(10, 10)
-probs <- matrix(c(1.0, 0.0, 0.0, 0.0), ncol=2, byrow=TRUE)
-m <- SBM$new(groups=groups, probs=probs)
-
-ave_out_degs <- vector(length=N)
-ave_in_degs <- vector(length=N)
-group_ave_in_degs <- matrix(ncol=2, nrow=N)
-group_ave_out_degs <- matrix(ncol=2, nrow=N)
-for(i in 1:N){
-  # make a realization of this graph model
-  n_stats <- network_stats(m$realize(), groups)
-  ave_out_degs[i] <- n_stats$ave_out_degree
-  ave_in_degs[i] <- n_stats$ave_in_degree
-  group_ave_out_degs[i,] <- n_stats$group_ave_out_degree
-  group_ave_in_degs[i,] <- n_stats$group_ave_in_degree
+mc_sim <- function(net_model, N, ...){
+  t_start = Sys.time()
+  retenv <- new.env()
+  retenv$ave_out_degs <- vector(length=N)
+  retenv$ave_in_degs <- vector(length=N)
+  retenv$group_ave_in_degs <- matrix(ncol=2, nrow=N)
+  retenv$group_ave_out_degs <- matrix(ncol=2, nrow=N)
+  retenv$ave_CB <- vector(length=N)
+  cat("Progress:")
+  for(i in 1:N){
+    # make a realization of this graph model
+    n_stats <- network_stats(net_model$realize(), ...)
+    retenv$ave_out_degs[i] <- n_stats$ave_out_degree
+    retenv$ave_in_degs[i] <- n_stats$ave_in_degree
+    retenv$group_ave_out_degs[i,] <- n_stats$group_ave_out_degree
+    retenv$group_ave_in_degs[i,] <- n_stats$group_ave_in_degree
+    retenv$ave_CB[i] <- mean(n_stats$CB)
+    cat("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b")
+    cat("Progress: ", round(i/N*100), "%, Elapsed:", round(Sys.time()-t_start, 1), " seconds", sep="")
+  }
+  cat(", Done\n")
+  return(retenv)
 }
 
-hist(group_ave_out_degs[,1])
-hist(group_ave_in_degs[,1])
+
+
+#specify some models
+n_groups <- 2
+groups <- c(20, 20)
+probs <- matrix(c(0.8, 0.2, 0.0, 0.0), ncol=2, byrow=TRUE)
+m <- SBM$new(groups=groups, probs=probs)
+
+probs <- matrix(runif(1600), ncol=40, byrow=TRUE)
+m2 <- GDI$new(groups=groups, probs=probs)
+
+N <- 10
+results <- mc_sim(m2, N, groups)
+
+hist(group_ave_out_degs[,1], breaks=50)
+hist(group_ave_in_degs[,1], breaks=50)
+hist(ave_CB, breaks=50)
