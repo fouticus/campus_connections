f75bb17ef85c30da748468a2f68c36e993f05c05
Alex Fout <fout.alex@gmail.com>
2017-12-19 15:40:13 -0700

    added code to run simulations for DI model for all semesters and nights

--- model_mc.R
+++ model_mc.R
@@ -34,6 +34,9 @@ plot_results(global_results, global_empirical, bins, c("Mentees", "Mentors"), sa
 
 
 # Stochastic Block Model with 2 blocks AND accounting for Condition vs. No Condition
+k <- 1
+results <- list(length=length(semesters)*length(nights))
+empirical <- list(length=length(semesters)*length(nights))
 for(sem in semesters){
   for(night in nights){
     t_start <- Sys.time()
@@ -51,3 +54,27 @@ ignore = c(stat_names[grep("stddev", stat_names)], stat_names[grep("ave", stat_n
 global_results <- row_summary(combine_envs(results, along=2, ignore=ignore))
 global_empirical <- row_summary(combine_envs(empirical, along=2, ignore=ignore))
 plot_results(global_results, global_empirical, bins, c("Mentees", "Mentors"), save=T, fprefix=sprintf("%s_%d_%s", "SBM2", N, "global"))
+
+
+# Dyad independent model with 2 blocks AND accounting for Condition vs. No Condition, and also whether sender/receiver are same gender/mentor family/dyad relationship
+N <- 500
+k <- 1
+results <- list(length=length(semesters)*length(nights))
+empirical <- list(length=length(semesters)*length(nights))
+for(sem in semesters){
+  for(night in nights){
+    t_start <- Sys.time()
+    cat(sprintf("%s: Semester: %s, Night: %s", t_start, sem, night))
+    modeling_results <- DI_modeling(night, sem, N, bins, par=T, cores=3)
+    results[[k]] <- modeling_results[[1]]
+    empirical[[k]] <- modeling_results[[2]]
+    cat(sprintf(", Duration: %s minutes\n", round(difftime(Sys.time(), t_start, units="min"), 2)))
+    k <- k + 1
+  }
+}
+stat_names <- names(results[[1]])
+ignore = c(stat_names[grep("stddev", stat_names)], stat_names[grep("ave", stat_names)])
+# combine results from each simulation
+global_results <- row_summary(combine_envs(results, along=2, ignore=ignore))
+global_empirical <- row_summary(combine_envs(empirical, along=2, ignore=ignore))
+plot_results(global_results, global_empirical, bins, c("Mentees", "Mentors"), save=T, fprefix=sprintf("%s_%d_%s", "DI", N, "global"))
