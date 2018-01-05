3d364e2d32a0907d2542ba5fd523a16275d3ae08
Alex Fout <fout.alex@gmail.com>
2018-01-05 15:05:23 -0700

    initial mod of model_mc, but need to test it

--- model_mc.R
+++ model_mc.R
@@ -1,15 +1,26 @@
-# Author: Alex Fout
-# Monte Carlo simulation of network models to evaluate quality of model fit
-# Computes various network characteristics and plots their distributions along with the empirical values
+########################################################################################
+## model_mc.R
+## Monte Carlo simulation of network models to evaluate quality of model fit
+## Computes various network characteristics and plots their distributions along with the empirical values
+########################################################################################
 
+# run config file
+source("config.R")
+
+# set random seed
 set.seed(983182)
-source("functions.R")
 
-### Simulate some SBM's based on the SBM LR we ran ###
-N <- 500
-bins <- 50
+##########################
+#### Helper Functions ####
+##########################
+source("mc_functions.R")  # in separate file for brevity
 
-# Stochastic Block Model with 2 blocks
+N <- 500  # Number of iterations
+bins <- 50  # number of bins to use when creating histograms
+
+################################################
+#### SBM with 2 blocks: mentors and mentees ####
+################################################
 # Simulate each night/semester separately (we think of all semesters/nights consisting of a superpopulation)
 k <- 1
 results <- list(length=length(semesters)*length(nights))
@@ -18,6 +29,7 @@ for(sem in semesters){
   for(night in nights){
     t_start <- Sys.time()
     cat(sprintf("%s: Semester: %s, Night: %s", t_start, sem, night))
+    # run the simulation
     modeling_results <- SBM_modeling(night, sem, N, bins, par=T, cores=3)
     results[[k]] <- modeling_results[[1]]
     empirical[[k]] <- modeling_results[[2]]
@@ -25,6 +37,7 @@ for(sem in semesters){
     k <- k + 1
   }
 }
+# make a list of all statistics that are averages and standard deviations. we don't want these in the global results
 stat_names <- names(results[[1]])
 ignore = c(stat_names[grep("stddev", stat_names)], stat_names[grep("ave", stat_names)])
 # combine results from each simulation
@@ -33,7 +46,9 @@ global_empirical <- row_summary(combine_envs(empirical, along=2, ignore=ignore))
 plot_results(global_results, global_empirical, bins, c("Mentees", "Mentors"), save=T, fprefix=sprintf("%s_%d_%s", "SBM", N, "global"))
 
 
-# Stochastic Block Model with 2 blocks AND accounting for Condition vs. No Condition
+##################################################################
+#### SBM with 2 blocks: mentors and mentees, and MF Condition ####
+##################################################################
 k <- 1
 results <- list(length=length(semesters)*length(nights))
 empirical <- list(length=length(semesters)*length(nights))
@@ -41,6 +56,7 @@ for(sem in semesters){
   for(night in nights){
     t_start <- Sys.time()
     cat(sprintf("%s: Semester: %s, Night: %s", t_start, sem, night))
+    # run the simulation
     modeling_results <- SBM2_modeling(night, sem, N, bins, par=T, cores=3)
     results[[k]] <- modeling_results[[1]]
     empirical[[k]] <- modeling_results[[2]]
@@ -48,6 +64,7 @@ for(sem in semesters){
     k <- k + 1
   }
 }
+# make a list of all statistics that are averages and standard deviations. we don't want these in the global results
 stat_names <- names(results[[1]])
 ignore = c(stat_names[grep("stddev", stat_names)], stat_names[grep("ave", stat_names)])
 # combine results from each simulation
@@ -56,7 +73,9 @@ global_empirical <- row_summary(combine_envs(empirical, along=2, ignore=ignore))
 plot_results(global_results, global_empirical, bins, c("Mentees", "Mentors"), save=T, fprefix=sprintf("%s_%d_%s", "SBM2", N, "global"))
 
 
-# Dyad independent model with 2 blocks AND accounting for Condition vs. No Condition, and also whether sender/receiver are same gender/mentor family/dyad relationship
+##############################################################################################################
+#### Dyad independent model with 2 blocka and MF condition, w/ factors for same gender/mentor-family/dyad ####
+##############################################################################################################
 N <- 500
 k <- 1
 results <- list(length=length(semesters)*length(nights))
@@ -65,6 +84,7 @@ for(sem in semesters){
   for(night in nights){
     t_start <- Sys.time()
     cat(sprintf("%s: Semester: %s, Night: %s", t_start, sem, night))
+    # run the sim
     modeling_results <- DI_modeling(night, sem, N, bins, par=T, cores=3)
     results[[k]] <- modeling_results[[1]]
     empirical[[k]] <- modeling_results[[2]]
@@ -72,6 +92,7 @@ for(sem in semesters){
     k <- k + 1
   }
 }
+# make a list of all statistics that are averages and standard deviations. we don't want these in the global results
 stat_names <- names(results[[1]])
 ignore = c(stat_names[grep("stddev", stat_names)], stat_names[grep("ave", stat_names)])
 # combine results from each simulation
