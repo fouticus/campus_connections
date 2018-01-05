057eba547d687d9103ac98282d8ce94797a5f3f1
Alex Fout <fout.alex@gmail.com>
2018-01-05 14:54:14 -0700

    added cleanup at end

--- edge_trajectories.R
+++ edge_trajectories.R
@@ -136,6 +136,16 @@ for(semester_a in semesters){
     dyad_trajectory(valid_edges, semester_a, night_a, scale_factor, point_size)
   }
 }
+
+#################
+#### Cleanup ####
+#################
 rm(dyad_trajectory)
 rm(plot_outedges_byperson)
 rm(plot_outedges_dyadonly)
+rm(alpha)
+rm(point_size)
+rm(night_a)
+rm(role_a)
+rm(scale_factor)
+rm(semester_a)
